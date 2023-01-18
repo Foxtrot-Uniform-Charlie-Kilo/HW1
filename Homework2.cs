e79a11ba37c91fd9b4f21ef7c51968699e0d879f
maxim <maximus77t@gmail.com>
2023-01-18 21:49:28 +0300

    Merge with task2

--- Homework2.cs
+++ Homework2.cs
@@ -11,3 +11,21 @@ int r1 = (num / 10)%10;
     Console.WriteLine(r1);
 }
 
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//645 -> 5
+//78 -> третьей цифры нет
+//32679 -> 6
+
+System.Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+string S1 = Convert.ToString(num);
+int S2 = S1.Length;
+if (S2 > 2)
+{
+    Console.WriteLine($"Третья цифра введённого числа: {S1[2]}");
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
